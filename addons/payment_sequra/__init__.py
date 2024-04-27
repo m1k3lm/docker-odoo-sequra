193529bfd4a35bb951b006bb0a96d802e77941be
Mikel Martin <mikel.martin@sequra.es>
2024-04-27 22:18:10 +0200

    precomit changes

--- addons/payment_sequra/__init__.py
+++ addons/payment_sequra/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from . import controllers
 from . import models
 
@@ -7,8 +5,8 @@ from odoo.addons.payment import setup_provider, reset_payment_provider
 
 
 def post_init_hook(env):
-    setup_provider(env, 'sequra')
+    setup_provider(env, "sequra")
 
 
 def uninstall_hook(env):
-    reset_payment_provider(env, 'sequra')
+    reset_payment_provider(env, "sequra")
